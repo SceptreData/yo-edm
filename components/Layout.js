5d9a1575fb52350907359db6e5b80d737b43e870
David Bergeron <david.f.bergeron@gmail.com>
2019-09-10 00:32:02 -0600

    Relaced reset with Ultra-lite reset

--- components/Layout.js
+++ components/Layout.js
@@ -1,90 +1,55 @@
-import Header from './Header';
+import Header from "./Header";
 
 export default function Layout(props) {
   return (
-    <div className='page-layout'>
+    <div className="page-layout">
       {/* <Header /> */}
       <main>{props.children}</main>
       <style jsx global>
         {`
           /* RESET */
-          /*! minireset.css v0.0.5 | MIT License | github.com/jgthms/minireset.css */
-          html,
-          body,
-          p,
-          ol,
-          ul,
-          li,
-          dl,
-          dt,
-          dd,
-          blockquote,
-          figure,
-          fieldset,
-          legend,
-          textarea,
-          pre,
-          iframe,
-          hr,
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6 {
-            margin: 0;
-            padding: 0;
-          }
-          h1,
-          h2,
-          h3,
-          h4,
-          h5,
-          h6 {
-            font-size: 100%;
-            font-weight: normal;
-          }
-          ul {
-            list-style: none;
-          }
-          button,
-          input,
-          select,
-          textarea {
-            margin: 0;
-          }
           html {
             box-sizing: border-box;
+            font-size: 100%;
           }
+
           *,
           *:before,
           *:after {
             box-sizing: inherit;
           }
-          img,
-          video {
-            height: auto;
-            max-width: 100%;
-          }
-          iframe {
-            border: 0;
-          }
-          table {
-            border-collapse: collapse;
-            border-spacing: 0;
-          }
-          td,
-          th {
+
+          body,
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6,
+          p,
+          ol,
+          ul {
+            margin: 0;
             padding: 0;
-            text-align: left;
+            font-weight: normal;
+          }
+
+          ol,
+          ul {
+            list-style: none;
+          }
+
+          img {
+            max-width: 100%;
+            height: auto;
           }
 
           /*
            * MY STYLES HERE
            */
           html {
-            font-family: 'Roboto', sans-serif;
-            background: url('/static/img/concert-big.jpg') no-repeat center
+            font-family: "Roboto", sans-serif;
+            background: url("/static/img/concert-big.jpg") no-repeat center
               center fixed;
             background-size: cover;
             color: #fff;
@@ -94,7 +59,7 @@ export default function Layout(props) {
             position: relative;
           }
           h1 {
-            font-family: 'Permanent Marker', sans-serif;
+            font-family: "Permanent Marker", sans-serif;
           }
           p {
             margin-bottom: 1.25em;
@@ -112,7 +77,7 @@ export default function Layout(props) {
           h4,
           h5 {
             margin: 2.75rem 0 1rem;
-            font-family: 'Roboto', sans-serif;
+            font-family: "Roboto", sans-serif;
             font-weight: 300;
             line-height: 1.15;
           }
