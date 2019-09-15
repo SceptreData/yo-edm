c5535cc71ae3d1b8d9ca16f9489a892335f6d31d
David Bergeron <david.f.bergeron@gmail.com>
2019-09-14 19:35:42 -0600

    reworked event list

--- components/EventItem.js
+++ components/EventItem.js
@@ -1,9 +1,9 @@
 function unpackEvent(event) {
   const { name, dates, images, url } = event;
-  const { genre, subgenre } = event.classifications[0];
+  const { genre, subGenre } = event.classifications[0];
   const venue = event._embedded.venues[0];
 
-  return { name, dates, images, url, genre, subgenre, venue };
+  return { name, dates, images, url, genre, subGenre, venue };
 }
 
 const EventItem = ({ src }) => {
@@ -12,30 +12,44 @@ const EventItem = ({ src }) => {
     <li>
       <div className='name'>
         <h3>{event.name}</h3>
-        <h4>{event.subgenre}</h4>
+        <h4>{event.subGenre.name}</h4>
       </div>
-      <div>
-        <time className='event-time'>{event.dates.start.localTime}</time>
-        <time className='event-date'>{event.dates.start.localDate}</time>
+      <div className='event-details'>
+        <div>
+          <time className='event-time'>{event.dates.start.localTime}</time>
+          <time className='event-date'>{event.dates.start.localDate}</time>
+        </div>
+        <a className='venue-link' href={event.url} title={event.venue.name}>
+          {event.venue.name}
+          <span>→</span>
+        </a>
       </div>
-      <a href={event.url}>
-        {event.venue.name}
-        <span>→</span>
-      </a>
       <style jsx>{`
         li {
           display: flex;
           align-items: center;
+          justify-content: space-between;
           border-bottom: 1px solid lightgrey;
+          padding: 0 0.25rem;
+        }
+
+        li:hover {
+          background: rgb(255, 255, 255, 0.15);
         }
         .name {
           width: 400px;
-          border-right: 1px solid lightgrey;
           font-size: 1.563em;
         }
         h3 {
+          font-size: 1.25rem;
           margin-top: 0.25rem;
           margin-bottom: 0.25rem;
+        }
+        h4 {
+          font-size: 0.8rem;
+        }
+        .event-details {
+          text-align: right;
         }
         time {
           display: block;
@@ -46,12 +60,12 @@ const EventItem = ({ src }) => {
           text-decoration: none;
         }
 
-        span {
-          font-size: 2rem;
-        }
-
         a:hover {
           color: goldenrod;
+        }
+
+        span {
+          font-size: 1.25rem;
         }
       `}</style>
     </li>
--- components/Events.js
+++ components/Events.js
@@ -2,6 +2,8 @@ import useFetch from './useFetch';
 import LoadingBars from './LoadingBars';
 import EventItem from './EventItem';
 
+import testEvents from '../data/testEvents';
+
 const Events = () => {
   const res = useFetch('https://yoedmonton.com/api/hiphop', {});
   // const res = useFetch('https://yo-edmonton.sceptre.now.sh/api/hiphop', {});
@@ -14,14 +16,14 @@ const Events = () => {
 
   // Display our list of events.
   const events = res.response._embedded.events;
+  // console.log(testEvents);
+  // const events = testEvents._embedded.events;
   return (
     <section className='root'>
       <h2>Events</h2>
       <ul>
         {events.map(event => (
-          <li>
-            <EventItem src={event} />
-          </li>
+          <EventItem src={event} key={event.id} />
         ))}
       </ul>
       <style jsx>{`
@@ -32,6 +34,9 @@ const Events = () => {
           background: rgba(0, 0, 0, 0.5);
           margin-bottom: 2rem;
           margin-right: 1rem;
+        }
+        h2 {
+          padding: 0 0.25rem;
         }
         ul {
           list-style: none;
